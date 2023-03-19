c781cbf11822f9911370703775fda5bc9edbed99
Leonid10011 <leonid.budkov@gmail.com>
2023-03-19 14:57:12 +0100

    Finished Section "React JSX"

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,25 @@ import './App.css'
 
 function App() {
   const title = "React";
+  const welcome = {
+    title: "React",
+    greeting: "Hey"
+  }
+
+  function getTitle(title){
+    return title;
+  }
+
+  const myList = [1,"8",2,3,4];
 
   return (
     <div>
-      <h1>Hello {title}</h1>
+      <h1>{welcome.greeting} {getTitle(welcome.title)}</h1>
+      <ul>
+        {myList.map(i => <li> {i} </li>)}
+      </ul> 
+      <label htmlFor="search">Search</label>
+      <input id="search" type="text" />
     </div>
   )
 }
