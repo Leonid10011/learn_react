4ab8db00c67c407b6ae52667467626fc9f55094e
Leonid10011 <leonid.budkov@gmail.com>
2023-03-23 21:26:23 +0100

    added eventListener

--- src/App.jsx
+++ src/App.jsx
@@ -50,10 +50,16 @@ const List = ({a}) => {
 }
 
 const Search = () => {
+  const handleChange = event => {
+    console.log(event);
+
+    console.log(event.target.value);
+  };
+
   return(
     <div>
       <label htmlFor="search">Search</label>
-      <input id="search" type="text" />
+      <input id="search" type="text" onChange={handleChange} />
     </div>
   );
 }
