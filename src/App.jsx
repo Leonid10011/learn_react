3354a603addea6f21cb926bc18836e2cd4f5b92b
Leonid10011 <leonid.budkov@gmail.com>
2023-03-31 17:15:16 +0200

    Moved Buttons and Slider to their own jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useState } from 'react'
 import './App.css'
-import styled from "styled-components"
 
 const style = {
   padding: "10px 30px",
@@ -80,11 +79,6 @@ const App = () => {
       <hr />
       <List a={filterList(stories, searchTerm)}/>
       <hr />
-      <Button id="btn1" name="Click Me" handleClick={onClick}/>
-      <hr />
-      <Dropdown value={dropdown} onClick={setDropdown}/>
-      <hr />
-      <Slider></Slider>
     </div>
   );
 }
@@ -149,128 +143,6 @@ const Element = ({title, url, author, num_comments, points}) => {
     </div>
   )
 }
-
-const Button = ({id, name, handleClick, ...rest}) => {
-  return(
-    <>
-      <button type="button" id={id} onClick={handleClick}>{name}</button>
-    </>
-  )
-}
-
-const RadioButton = ({label, value, onChange}) => {
-  return(
-    <>
-      <input type="radio" value={value} onChange={onChange} />
-      {label}
-    </>
-  );
-}
-
-const CheckButton = ({label, value, onChange}) => {
-  return(
-    <>
-      <input type="checkbox" value={value} onChange={onChange}/>
-      {label}
-    </>
-  );
-}
-
-const Dropdown = ({value, onClick}) => {
-  const handleClick = (event) => {
-    onClick(!value);
-  }
-
-  return(
-    <>
-    <button onClick={handleClick}>Dropdown</button>
-      {value ? (
-      <ul className="menu">
-        <li className="menu-item">
-          <button>Menu 1</button>
-        </li>
-        <li className="menu-item">
-          <button>Menu 2</button>
-        </li>
-      </ul>
-      ) : null}
-      {value ? <div>Is Open</div> : <div>Is Closed</div>}
-    </>
-  );
-}
-
-const StyleSlider = styled.div`
-  position: relative;
-  border-radius: 3px;
-  background: #dddddd;
-  height: 15px;
-`;
-
-const SliderThumb = styled.div`
-  width: 10px;
-  height: 25px;
-  border-radius: 3px;
-  position: relative;
-  top: -5px;
-  opacity: 0.5;
-  background: #823eb7;
-  cursor: pointer;
-`;
-
-const getPercentage = (current, max) => (100*current) / max;
-
-const getLeft = percentage => `calc(${percentage}% - 5px)`;
-
-const Slider = () => {
-  const sliderRef = React.useRef()
-  const thumbRef = React.useRef()
-
-  const diff = React.useRef();
-
-  const handleMouseMove = event => {
-    let newX = 
-      event.clientX -
-      diff.current -
-      sliderRef.current.getBoundingClientRect().left;
-
-      const end =  sliderRef.current.offsetWidth - thumbRef.current.offsetWidth;
-
-      const start = 0;
-
-      if(newX < start){
-        newX = 0;
-      }
-
-      if(newX > end){
-        newX = end;
-      }
-
-      const newPercentage = getPercentage(newX, end);
-      thumbRef.current.style.left = getLeft(newPercentage);
-  }
-
-  const handleMouseUp = () => {
-    document.removeEventListener("mouseup", handleMouseUp);
-    document.removeEventListener("mousemove", handleMouseMove);
-  }
-
-  const handleMouseDown = event => {
-    diff.current = 
-      event.clientX - thumbRef.current.getBoundingClientRect().left;
-    
-    document.addEventListener("mousemove", handleMouseMove);
-    document.addEventListener("mouseup", handleMouseUp);
-  }
-
-  return(
-    <>
-      <StyleSlider ref={sliderRef}>
-        <SliderThumb ref={thumbRef} onMouseDown={handleMouseDown}/>
-      </StyleSlider>
-    </>
-  );
-}
-
 
 
 
